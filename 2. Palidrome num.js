17c4aee78b347ff284b0a19bb7e34af6960ad5b7
ericcnguyen04 <ericc.win@gmail.com>
2023-04-25 15:59:35 -0400

    starting palidrome

--- 2. Palidrome num.js
+++ 2. Palidrome num.js
@@ -1,2 +1,14 @@
-Given an integer x, return true if x is a palindrome, and false otherwise.
+// Given an integer x, return true if x is a palindrome, and false otherwise.
 
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+};
