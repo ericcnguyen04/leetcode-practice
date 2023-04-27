4404651d067645bae265e8f4f8eb8fa205a79f06
ericcnguyen04 <ericc.win@gmail.com>
2023-04-26 20:55:17 -0400

    set up

---
+++ 3. Length of last word.js
@@ -0,0 +1,9 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+const string = "ta da! This is my final word"
+
+const lastWord = (str) => {
+    str.split(' ')
+}
