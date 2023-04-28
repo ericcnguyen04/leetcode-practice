55ddc7a4d764fd2e46dd73ca097191887a5d88fd
ericcnguyen04 <ericc.win@gmail.com>
2023-04-28 12:34:34 -0400

    sudo code

--- 5. Single Number.js
+++ 5. Single Number.js
@@ -2,3 +2,16 @@
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+myArr = [5, 3, 3, 9, 2, 9, 2]
+
+const singleNumber = (arr) => {
+    // loop through array
+
+    // find the number with no duplicate
+
+    // return the number
+
+
+}
+
+singleNumber(myArr)
