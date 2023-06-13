ec39851b2b32ef334ae7302a3d7920c18fb1873b
ericcnguyen04 <ericc.win@gmail.com>
2023-06-12 23:21:06 -0400

    setting up function

--- 4. Keyboard Row.js
+++ 4. Keyboard Row.js
@@ -5,7 +5,14 @@
 const invalid = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
 
 // loop through script
+const script = 'glad'
 
 // if word had invalid characters, return invalid
+const validation = (word, condition) => {
+    for (let i = 0; i < word.length; i++) {
+        console.log(word)
+    }
+}
 
 // return validation
+validation(script)
