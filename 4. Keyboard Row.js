da5f0a80f4981d67ca3258e0bdef8c045bd8f196
ericcnguyen04 <ericc.win@gmail.com>
2023-06-12 23:15:10 -0400

    sudo code

--- 4. Keyboard Row.js
+++ 4. Keyboard Row.js
@@ -1,3 +1,10 @@
 // Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard 
 // iow, only display words that use the middle of the keyboard
 
+// make an array of invalid characters
+
+// loop through script
+
+// if word had invalid characters, return invalid
+
+// return validation
