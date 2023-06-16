29349707131501c8575ffe6b6cc6cd5c7f4472d4
ericcnguyen04 <ericc.win@gmail.com>
2023-06-15 22:55:53 -0400

    single number completed

--- 5. Single Number.js
+++ 5. Single Number.js
@@ -4,15 +4,30 @@
 
 myArr = [5, 3, 3, 9, 2, 9, 2]
 
-const singleNumber = (arr) => {
-    // loop through array
-    arr.filter((item, index) => arr.indexOf(item) !== index)
-    
-    // find the number with no duplicate
 
-    // return the number
-
-
-}
-
-singleNumber(myArr)
+function findNonDuplicatedNumber(arr) {
+    const freqMap = new Map(); // Map to store the frequency of each element
+  
+    // Count the frequency of each element
+    for (let i = 0; i < arr.length; i++) {
+      const num = arr[i];
+      if (freqMap.has(num)) {
+        freqMap.set(num, freqMap.get(num) + 1);
+      } else {
+        freqMap.set(num, 1);
+      }
+    }
+  
+    // Find the non-duplicated number
+    for (const [num, freq] of freqMap.entries()) {
+      if (freq === 1) {
+        return num;
+      }
+    }
+  
+    return null; // Return null if there's no non-duplicated number
+  }
+  
+  // Example usage
+  const nonDuplicatedNumber = findNonDuplicatedNumber(myArr);
+  console.log(nonDuplicatedNumber);
